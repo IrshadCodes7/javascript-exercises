c09c5b131dd17d9364e053fe1a14ce784349e489
IrshadCodes7 <irshadmd92236@gmail.com>
2025-09-20 20:44:39 +0530

    Solve

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    startType=typeof(start)
+    endType=typeof(end)
 
+    if(startType!="number" || endType!="number"){
+        return "ERROR"
+    }
+    if(parseInt(start)!=start || parseInt(end)!=end){
+        return "ERROR"
+    }
+    if(start<0 ||end<0){
+        return "ERROR"
+    }
+    
+    if(start>end){
+        [start,end]=[end,start]
+    }
+    let sum=0
+    for(let num=start;num<=end;num++){
+        sum+=num
+    }
+    return sum
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
