c270620a5412535a28d9f93c782dec456b84f665
IrshadCodes7 <irshadmd92236@gmail.com>
2025-09-25 21:20:12 +0530

    Solve

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if(nums.length==0){
+    return 0
+  }
+
+	return nums.reduce((s,num)=>s+num)
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+    return nums.reduce((m,num)=>m*num)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact=1
+  for(let i=1;i<=num;i++){
+    fact*=i
+  }
+  return fact
 };
 
 // Do not edit below this line
